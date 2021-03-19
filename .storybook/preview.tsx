1e1d12f28094130bc52d2fe58dfd9ba71a5d43f6
kharann <anhkhav@gmail.com>
2021-03-19 14:22:59 +0100

    create-react-app with chakra and storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { addParameters, StoryContext } from "@storybook/react";
+import React from "react";
+
+const withChakra = (StoryFn: Function, _: StoryContext) => {
+  return (
+    <>
+      <ChakraProvider>
+        {/* <Fonts /> */}
+        <StoryFn />
+      </ChakraProvider>
+    </>
+  );
+};
+
+export const decorators = [withChakra];
+
+addParameters({
+  viewMode: "canvas",
+});
