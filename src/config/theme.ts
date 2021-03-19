41dbf8fe885d58bc5db5de6f1174d15a2ba3615f
kharann <anhkhav@gmail.com>
2021-03-19 14:41:04 +0100

    Delete unencesarry files from CRA

---
+++ src/config/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+// TODO: Define colors and theme based on design
+const theme = extendTheme({
+  colors: {
+    brand: {
+      500: "fefefe",
+    },
+  },
+});
+export default theme;
