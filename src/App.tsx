f873d17b6fba43602452d61930903c49897453e5
kharann <anhkhav@gmail.com>
2021-03-19 15:05:58 +0100

    Add prettier and format

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Center } from "@chakra-ui/react";
+import React from 'react';
+import { Button, Center } from '@chakra-ui/react';
 
 function App() {
   return (
