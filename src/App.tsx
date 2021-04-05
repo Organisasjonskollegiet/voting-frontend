d75e95d1224dffc0c1ed8529ef3e7fc9dc7026e8
kharann <anhkhav@gmail.com>
2021-04-05 03:05:51 +0200

    update example query

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PageExample from './components/pages/PageExample';
-import Auth0WithHistoryProvider from './services/providers/Auth0ProviderWithHistory';
+import Auth0WithHistoryProvider from './services/auth/Auth0ProviderWithHistory';
 
+// The Auth0 provider is here so it can access to the router hooks for location and history
 const App: FC = () => {
   return (
     <Router>
       <Auth0WithHistoryProvider>
-      <Switch>
-        <Route path="/admin">
-          <PageExample page="/admin"/>
-        </Route>
-        <Route path="/">
-          <PageExample page="/" />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path="/admin">
+            <PageExample page="/admin" />
+          </Route>
+          <Route path="/">
+            <PageExample page="/" />
+          </Route>
+        </Switch>
       </Auth0WithHistoryProvider>
     </Router>
   );
