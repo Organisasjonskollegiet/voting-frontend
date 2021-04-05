24c51e0a371e6a1d195930fd72b178c650b8efb9
kharann <anhkhav@gmail.com>
2021-04-05 19:36:52 +0200

    prefix env variable with react_app

--- src/services/apollo/client.ts
+++ src/services/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
   credentials: 'include', // important to include cookies
   cache: new InMemoryCache(),
 });
