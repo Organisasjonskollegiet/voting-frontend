cf50cf12f3e05445f1f83b1d07bf004cf7d44e0c
kharann <anhkhav@gmail.com>
2021-04-05 20:05:48 +0200

    Dont use dynamic build url in production

--- src/services/auth/Auth0ProviderWithHistory.tsx
+++ src/services/auth/Auth0ProviderWithHistory.tsx
@@ -6,9 +6,11 @@ const Auth0WithHistoryProvider: React.FC = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? '';
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? '';
   // Changes the callback url to the previewed deployment
-  const callbackUrl = process.env.REACT_APP_NETLIFY
-    ? process.env.REACT_APP_DEPLOY_URL
-    : process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const buildCtx = process.env.REACT_APP_CONTEXT;
+  const callbackUrl =
+    buildCtx == 'deploy-preview' || buildCtx == 'branch-deploy'
+      ? process.env.REACT_APP_DEPLOY_URL
+      : process.env.REACT_APP_AUTH0_CALLBACK_URL;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
   const history = useHistory();
