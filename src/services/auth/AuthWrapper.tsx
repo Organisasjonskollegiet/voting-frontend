a1c0ec4217fa13ca8ee105dee5ae1a140faeea17
kharann <anhkhav@gmail.com>
2021-04-05 03:04:55 +0200

    Mov auth0 and apollo to their corresponding services

--- src/services/auth/AuthWrapper.tsx
+++ src/services/auth/AuthWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Center, Spinner } from '@chakra-ui/react';
 
