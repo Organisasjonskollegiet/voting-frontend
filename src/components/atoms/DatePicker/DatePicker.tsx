c6e8d31c775de4d897d4caa21d6792c5ea94e715
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-08-18 13:25:05 +0200

    Use norwegian calendar

--- src/components/atoms/DatePicker/DatePicker.tsx
+++ src/components/atoms/DatePicker/DatePicker.tsx
@@ -1,5 +1,6 @@
 import React, { HTMLAttributes } from 'react';
-import ReactDatePicker from 'react-datepicker';
+import ReactDatePicker, { registerLocale } from 'react-datepicker';
+import nb from 'date-fns/locale/nb';
 import { Input, InputGroup, InputRightElement, useColorMode } from '@chakra-ui/react';
 import CalendarIcon from './icon.svg';
 import { inputStyle } from '../../particles/formStyles';
@@ -15,13 +16,13 @@ interface Props {
 }
 
 const CustomInput = (props: React.HTMLProps<HTMLInputElement>, ref: React.Ref<HTMLInputElement>) => {
-    return (
-      <InputGroup width='100%' sx={inputStyle}>
-        <Input value={props.value} placeholder={props.placeholder} onClick={props.onClick} onChange={props.onChange} />
-        <InputRightElement onClick={props.onClick} children={<img alt="calendar" src={CalendarIcon} />} />
-      </InputGroup>
-    );
-  };
+  return (
+    <InputGroup width="100%" sx={inputStyle}>
+      <Input value={props.value} placeholder={props.placeholder} onClick={props.onClick} onChange={props.onChange} />
+      <InputRightElement onClick={props.onClick} children={<img alt="calendar" src={CalendarIcon} />} />
+    </InputGroup>
+  );
+};
 
 const DatePicker: React.FC<Props & HTMLAttributes<HTMLElement>> = ({
   selectedDate,
@@ -30,19 +31,21 @@ const DatePicker: React.FC<Props & HTMLAttributes<HTMLElement>> = ({
   showPopperArrow = false,
   ...props
 }) => {
-  const isLight = useColorMode().colorMode==='light';//you can check what theme you are using right now however you want
+  const isLight = useColorMode().colorMode === 'light'; //you can check what theme you are using right now however you want
+  registerLocale('nb', nb);
   return (
-    // if you don't want to use chakra's colors or you just wwant to use the original ones, 
+    // if you don't want to use chakra's colors or you just wwant to use the original ones,
     // set className to "light-theme-original" ↓↓↓↓
-    <div className={isLight?"light-theme":"dark-theme"}>
+    <div className={isLight ? 'light-theme' : 'dark-theme'}>
       <ReactDatePicker
-        dateFormat='HH:mm dd.MM.yyyy'
+        dateFormat="HH:mm dd.MM.yyyy"
         selected={selectedDate}
         onChange={onChange}
+        locale="nb"
         isClearable={isClearable}
         showPopperArrow={showPopperArrow}
         showTimeInput
-        className="react-datapicker__input-text"//input is white by default and there is no already defined class for it so I created a new one
+        className="react-datapicker__input-text" //input is white by default and there is no already defined class for it so I created a new one
         customInput={React.createElement(React.forwardRef(CustomInput))}
       />
     </div>
