4b426c66d9eef744c681b0c7c1903b928dce3188
André Schjøth <aschjoth@gmail.com>
2021-08-26 14:16:33 +0200

    removed printing of token

--- src/services/apollo/ApolloProvider.tsx
+++ src/services/apollo/ApolloProvider.tsx
@@ -36,7 +36,6 @@ const ApolloAuthProvider: React.FC = ({ children }) => {
 
   const authLink = setContext(async () => {
     const token = await getAccessTokenSilently();
-    console.log(token);
     return {
       headers: {
         Authorization: `Bearer ${token}`,
