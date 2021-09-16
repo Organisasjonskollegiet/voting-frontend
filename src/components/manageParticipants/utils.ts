ffb94a07230193d488f1d4b2e7e6563b718f5ee1
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-09-16 14:19:39 +0200

    explicit typing

--- src/components/manageParticipants/utils.ts
+++ src/components/manageParticipants/utils.ts
@@ -1,4 +1,4 @@
-export const checkIfEmailIsValid = (email: string) => {
+export const checkIfEmailIsValid: (email: string) => boolean = (email: string) => {
   const emailRegExp = new RegExp(
     // eslint-disable-next-line no-useless-escape
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
