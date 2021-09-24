312ffc2543bc69fc6a6ba541e93371e3a32f521b
André Schjøth <aschjoth@gmail.com>
2021-09-24 15:24:25 +0200

    update votation type text

--- src/components/votationList/forms/VotationTypeInformation.tsx
+++ src/components/votationList/forms/VotationTypeInformation.tsx
@@ -9,27 +9,38 @@ const VotationTypeInformation: React.FC = () => {
       <Heading as="h2" fontSize="1.1rem">
         Simpelt Flertall
       </Heading>
-      <Text mb="1rem">Alternativet med flest stemmer vinner.</Text>
+      <Text mb="1rem">
+        Alternativet med flest stemmer vinner. <br />
+        Blanke stemmer telles ikke.
+      </Text>
       <Heading as="h2" fontSize="1.1rem">
         Kvalifisert flertall
       </Heading>
-      <Text mb="1rem">Et alternativ trenger mer enn halvparten stemmene for å vinne.</Text>
+      <Text mb="1rem">
+        Et alternativ trenger stemmene fra mer enn halvparten av de stemmeberettigede tilstede for å vinne.
+      </Text>
 
       <Heading as="h2" fontSize="1.1rem">
         Kvalifisert 2/3 flertall
       </Heading>
-      <Text mb="1rem">Et alternativ trenger mer enn 2/3 av stemmene for å vinne.</Text>
-
+      <Text mb="1rem">
+        Et alternativ trenger stemmene fra mer enn to tredjedeler av de stemmeberettigede tilstede for å vinne.
+      </Text>
       <Heading as="h2" fontSize="1.1rem">
         Preferansevalg
       </Heading>
       <Text mb="1rem">
         Velgerne rangerer alternativene i den rekkefølgen de foretrekker dem. Her har man mulighet til å velge flere
-        vinnere.
+        vinnere, men ikke muligheten til å stemme blankt.
       </Text>
       <Text>
         Hvordan resultatet blir utregnet kan du se her:{' '}
-        <Link href="https://www.youtube.com/watch?v=bLH_w5kHJpA" color="black" isExternal>
+        <Link
+          href="https://www.youtube.com/watch?v=bLH_w5kHJpA"
+          textDecoration="underline"
+          fontStyle="italic"
+          isExternal
+        >
           Single transferable vote <ExternalLinkIcon mx="2px" />
         </Link>
       </Text>
