692631967b9cb598f109e197f03475cf795bef4f
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-09-26 18:37:15 +0200

    Fix uploading same file without refresh, and move onFileUpload

--- src/components/manageParticipants/atoms/InviteParticipantByFileUpload.tsx
+++ src/components/manageParticipants/atoms/InviteParticipantByFileUpload.tsx
@@ -1,16 +1,18 @@
 import { FormLabel, HStack, Input, Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useRef } from 'react';
 import { inputStyle } from '../../styles/formStyles';
 import UploadIcon from '../../../static/uploadIcon.svg';
 
 interface InviteParticipantByFileUploadProps {
-  handleFileUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleFileUpload: (event: React.ChangeEvent<HTMLInputElement>, ref: React.RefObject<HTMLInputElement>) => void;
 }
 
 const InviteParticipantByFileUpload: React.FC<InviteParticipantByFileUploadProps> = ({ handleFileUpload }) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
     <FormLabel maxWidth="320px" w="30vw">
-      <Input display="none" type="file" accept="text/csv" onChange={handleFileUpload} />
+      <Input ref={ref} display="none" type="file" accept="text/csv" onChange={(e) => handleFileUpload(e, ref)} />
       <HStack sx={inputStyle} _hover={{ cursor: 'pointer' }} padding="8px" justify="center" borderRadius="4px">
         <img alt="upload" src={UploadIcon} />
         <Text>Last opp deltagerliste fra CSV-fil</Text>
