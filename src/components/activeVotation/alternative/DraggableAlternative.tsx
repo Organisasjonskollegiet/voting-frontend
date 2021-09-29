a33f1a62d9cb4e7bbb0aae18a2b8318847ef6916
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-09-29 09:03:28 +0200

    Use py instead of p

--- src/components/activeVotation/alternative/DraggableAlternative.tsx
+++ src/components/activeVotation/alternative/DraggableAlternative.tsx
@@ -14,13 +14,7 @@ const DraggableAlternative: React.FC<DraggableAlternativeProps> = ({ alternative
   return (
     <Draggable key={alternative.id} draggableId={alternative.id} index={alternative.index}>
       {(provided) => (
-        <HStack
-          p="0.5rem 0"
-          w="100%"
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
+        <HStack py="0.5rem" w="100%" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
           <HStack w="100%">
             <Text w="30px" fontSize="24px" fontWeight="bold" opacity="0.5">
               {alternative.index + 1}
