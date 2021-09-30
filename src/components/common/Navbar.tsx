96e42b1cceac808d8f98a46151fff94d15441d81
André Schjøth <aschjoth@gmail.com>
2021-09-30 12:48:20 +0200

    redesign of hamburger menu

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,8 +1,18 @@
 import React, { ReactNode } from 'react';
-import { Box, Flex, /*Avatar,*/ HStack, IconButton, useDisclosure, Stack, Image, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  /*Avatar,*/ HStack,
+  IconButton,
+  useDisclosure,
+  Stack,
+  Image,
+  Button,
+  Divider,
+} from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-import { darkblue, lightblue } from '../styles/theme';
-import { /*Link,*/ NavLink } from 'react-router-dom';
+import { darkblue } from '../styles/theme';
+import { NavLink } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import Logo from '../../static/logo.svg';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -15,11 +25,9 @@ const links: Map<string, string> = new Map([
 const pageNames = Array.from(links.keys());
 
 const NavigationLink = ({ children, link }: { children: ReactNode; link: string }) => (
-  <Box color={{ base: lightblue, md: darkblue }} _hover={{ color: { base: '#c6ccd5', md: '#9ca6b6' } }}>
-    <NavLink style={{ fontWeight: 'bold' }} to={link}>
-      {children}
-    </NavLink>
-  </Box>
+  <NavLink style={{ fontWeight: 'bold' }} to={link}>
+    <Button variant="link">{children}</Button>
+  </NavLink>
 );
 
 const Navbar: React.FC = () => {
@@ -28,25 +36,18 @@ const Navbar: React.FC = () => {
   const history = useHistory();
 
   return (
-    <Box
-      bg={{ base: darkblue, md: 'white' }}
-      px="50px"
-      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.05)"
-      zIndex="2"
-      position="relative"
-    >
+    <Box bg="white" px="50px" boxShadow="0px 4px 4px rgba(0, 0, 0, 0.05)" zIndex="2" position="relative">
       <Flex as="nav" h="5.5rem" alignItems="center" justifyContent="space-between">
         <Image
           _hover={{ cursor: 'pointer' }}
           onClick={() => history.push('/')}
-          display={{ base: 'none', md: 'flex' }}
           src={Logo}
           alt="Organisasjonskollegiet"
           h="3em"
           w="100px"
         />
 
-        <HStack as={'nav'} spacing="5.5em" display={{ base: 'none', md: 'flex' }}>
+        <HStack as={'nav'} spacing="3.5em" display={{ base: 'none', md: 'flex' }}>
           {pageNames.map((page) => (
             <NavigationLink key={page} link={links.get(page) || ''}>
               {page}
@@ -58,16 +59,22 @@ const Navbar: React.FC = () => {
         {/* <Link to={links.get('Min profil') || ''}>
             <Avatar size="sm" />
           </Link> */}
-        <Button w="100px" onClick={() => logout({ returnTo: window.location.origin })}>
+        <Button
+          w="100px"
+          onClick={() => logout({ returnTo: window.location.origin })}
+          display={{ base: 'none', md: 'flex' }}
+        >
           Logg ut
         </Button>
         {/* </Box> */}
 
         {/* Button to toggle hamburger menu */}
         <IconButton
-          bg={darkblue}
+          bg="white"
           size={'md'}
-          icon={isOpen ? <CloseIcon color="white" boxSize="1em" /> : <HamburgerIcon color="white" boxSize="1.5em" />}
+          icon={
+            isOpen ? <CloseIcon color={darkblue} boxSize="1em" /> : <HamburgerIcon color={darkblue} boxSize="1.5em" />
+          }
           aria-label={'Open Menu'}
           display={{ md: 'none' }}
           onClick={isOpen ? onClose : onOpen}
@@ -77,12 +84,16 @@ const Navbar: React.FC = () => {
       {/* The hamburger menu */}
       {isOpen && (
         <Box pb={4} display={{ md: 'none' }}>
-          <Stack as={'nav'} spacing={4} pl="0.5em">
+          <Divider mb="1rem" />
+          <Stack as={'nav'} spacing={2} pl="0.5em">
             {pageNames.map((page) => (
               <NavigationLink key={page} link={links.get(page) || ''}>
                 {page}
               </NavigationLink>
             ))}
+            <Button maxW="max-content" variant="link" onClick={() => logout({ returnTo: window.location.origin })}>
+              Logg ut
+            </Button>
           </Stack>
         </Box>
       )}
