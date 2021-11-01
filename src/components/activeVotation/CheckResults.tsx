bc82c1738563a8b87f28f6b9ac68c4ec7f5543c8
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-11-01 18:13:22 +0100

    possible to download result of stv

--- src/components/activeVotation/CheckResults.tsx
+++ src/components/activeVotation/CheckResults.tsx
@@ -52,28 +52,73 @@ const CheckResults: React.FC<CheckResultsProps> = ({
     history.push(`/meeting/${meetingId}`);
   };
 
-  console.log(stvResult);
-
   const getStvOverview = () => {
     if (!stvResult?.getStvResult) throw new Error('Fant ikke resultat.');
-    return `Antall stemmer som krevdes for å vinne: ${stvResult.getStvResult.quota} \n
-    Antall stemmeberettigede deltakere: ${stvResult.getStvResult.votingEligibleCount} \n
-    Antall avgitte stemmer: ${stvResult.getStvResult.voteCount}`;
+    return (
+      `Antall stemmer som krevdes for å vinne: ${stvResult.getStvResult.quota}` +
+      '\n' +
+      `Antall stemmeberettigede deltakere: ${stvResult.getStvResult.votingEligibleCount}` +
+      '\n' +
+      `Antall avgitte stemmer: ${stvResult.getStvResult.voteCount} \n`
+    );
+  };
+
+  const formatEliminatedText = (eliminated: string[], eliminatedAs: 'Vinner' | 'Taper') => {
+    if (eliminated.length === 0) return '';
+    switch (eliminated.length) {
+      case 0:
+        return '';
+      case 1:
+        return `${eliminatedAs}: ${eliminated[0]}`;
+      case 2:
+        return `${eliminatedAs}e: ${eliminated[0]} og ${eliminated[1]}`;
+      default:
+        return `${eliminatedAs}e: ${
+          eliminated.slice(eliminated.length - 2).reduce((a, b) => a + ', ' + b) +
+          ' og ' +
+          eliminated[eliminated.length - 1]
+        }`;
+    }
+  };
+
+  const getRoundEliminatedFileContent = (winners: string[], losers: string[]) => {
+    if (winners.length > 0) {
+      return formatEliminatedText(winners, 'Vinner');
+    } else if (losers.length > 0) {
+      return formatEliminatedText(losers, 'Taper');
+    } else {
+      return '';
+    }
   };
 
   const getStvRoundResultFileContent = (
     index: number,
-    eliminated: string,
+    winners: string[],
+    losers: string[],
     alternatives: { text: string; votes: number }[]
   ) => {
-    const header = `Runde ${index + 1}`;
-    return '';
+    const header = `Runde ${index + 1} \n`;
+    const eliminated = getRoundEliminatedFileContent(winners, losers) + '\n';
+    const alternativesHeader = 'Alternativ, Antall stemmer \n';
+    const alternativesContent = alternatives.map((a) => `${a.text}, ${a.votes} \n`);
+    return header + eliminated + alternativesHeader + alternativesContent.reduce((a, b) => a + b);
   };
 
   const getStvResultFileContent = () => {
-    const overView = getStvOverview();
-
-    return '';
+    const overview = getStvOverview() + '\n';
+    const rounds = stvResult?.getStvResult?.stvRoundResults.map(
+      (round) =>
+        getStvRoundResultFileContent(
+          round.index,
+          round.winners.map((w) => w.text),
+          round.losers.map((l) => l.text),
+          round.alternativesWithRoundVoteCount.map((a) => {
+            return { text: a.alternative.text, votes: a.voteCount };
+          })
+        ) + '\n'
+    );
+    console.log(rounds);
+    return overview + rounds?.reduce((a, b) => a + b);
   };
 
   const getAlternativeResultString = (text: string, votes: number) => {
@@ -91,7 +136,7 @@ const CheckResults: React.FC<CheckResultsProps> = ({
 
   const getResultFileContent = () => {
     if (isStv) {
-      return '';
+      return getStvResultFileContent();
     }
     if (!result?.getVotationResults) throw new Error('Fant ikke resultatene fra voteringen.');
     const header = 'Alternativ, Antall stemmer, % av stemmene, % av stemmeberettigede \n';
