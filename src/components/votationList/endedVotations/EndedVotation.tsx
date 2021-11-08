de3b02769f29ed1aea22735e6419edfa85daa436
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-11-08 20:41:34 +0100

    Use tooltip instead of accordion

--- src/components/votationList/endedVotations/EndedVotation.tsx
+++ src/components/votationList/endedVotations/EndedVotation.tsx
@@ -1,17 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import EndedVotationTemplate, { EndedVotationProps } from './EndedVotationTemplate';
-import {
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-  HStack,
-  Text,
-  Tooltip,
-} from '@chakra-ui/react';
+import { Box, Text, Tooltip } from '@chakra-ui/react';
 import { collapsedStyle } from '../../styles/formStyles';
-import AlternativesString from '../../common/AlternativesString';
 
 const EndedVotation: React.FC<EndedVotationProps> = ({ votation, duplicateVotation, role }) => {
   const [isOverflown, setIsOverflown] = useState(false);
@@ -22,7 +12,7 @@ const EndedVotation: React.FC<EndedVotationProps> = ({ votation, duplicateVotati
   const winnerString =
     numberOfWinners > 0
       ? winners.map(
-          (a, index) => `${a.text}${index !== votation.alternatives.filter((a) => a.isWinner).length - 1 ? ', ' : ''}`
+          (a, index) => `${a.text}${index < winners.length - 2 ? ', ' : index === winners.length - 2 ? ' og ' : ''}`
         )
       : 'Ingen vinner';
 
@@ -39,29 +29,7 @@ const EndedVotation: React.FC<EndedVotationProps> = ({ votation, duplicateVotati
     marginBottom: '1.5em',
   };
 
-  return numberOfWinners > 1 ? (
-    <AccordionItem key={votation.id} sx={styles}>
-      <EndedVotationTemplate votation={votation} role={role} duplicateVotation={duplicateVotation}>
-        <AccordionButton p="1em 0">
-          <Tooltip label={winnerString} isDisabled={!isOverflown}>
-            <Text ref={ref} isTruncated maxWidth="150px">
-              {winnerString}
-            </Text>
-          </Tooltip>
-          <AccordionIcon />
-        </AccordionButton>
-      </EndedVotationTemplate>
-
-      <AccordionPanel>
-        <HStack alignItems="flex-end" wrap="wrap">
-          <Text fontWeight="bold" fontSize="1rem">
-            Vinnere:
-          </Text>
-          <AlternativesString fontWeight="normal" alternatives={winners.map((w) => w.text)} />
-        </HStack>
-      </AccordionPanel>
-    </AccordionItem>
-  ) : (
+  return (
     <Box key={votation.id} sx={styles}>
       <EndedVotationTemplate votation={votation} duplicateVotation={duplicateVotation} role={role}>
         <Tooltip label={winnerString} isDisabled={!isOverflown}>
