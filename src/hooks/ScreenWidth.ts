035ea96dfbb5db736951211aaf8636c1da7795fe
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-11-21 23:54:28 +0100

    Fix lint

--- src/hooks/ScreenWidth.ts
+++ src/hooks/ScreenWidth.ts
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function useScreenWidth() {
+function useScreenWidth(): number {
   const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
 
   useEffect(() => {
