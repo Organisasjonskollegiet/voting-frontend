a8ab5a86b21e04ea7d602d8fc6887dcd333a30b6
André Schjøth <aschjoth@gmail.com>
2022-01-13 23:41:51 +0100

    Adjust placement of navigational buttons on mobile

--- src/components/manageMeeting/ManageMeetingController.tsx
+++ src/components/manageMeeting/ManageMeetingController.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Divider, VStack, Grid, Button, Box, Text } from '@chakra-ui/react';
+import { Divider, VStack, Grid, Button, Text } from '@chakra-ui/react';
 import ArrowLeft from '../../static/arrowLeft.svg';
 import ManageMeetingStatus from './ManageMeetingStatus';
+import useScreenWidth from '../../hooks/ScreenWidth';
 
 interface IProps {
   handleNavigation: (nextIndex: number) => void;
@@ -10,26 +11,28 @@ interface IProps {
 }
 
 const ManageMeetingController: React.FC<IProps> = ({ handleNavigation, showPrev, activeTab }) => {
+  const screenWidth = useScreenWidth();
+
   return (
     <>
       <Divider m="3em 0" />
       <VStack spacing="16">
-        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-          {showPrev ? (
+        <Grid templateColumns="repeat(3, 1fr)" templateRows="reapeat(2, 1fr)" gap="2">
+          {showPrev && (
             <Button
               variant="standard"
               leftIcon={<img alt="previous" src={ArrowLeft} />}
               maxWidth="100px"
               onClick={() => handleNavigation(activeTab - 1)}
+              gridArea={screenWidth > 600 ? '2 / 1' : '1 / 2'}
+              justifySelf={screenWidth > 600 ? 'left' : 'center'}
             >
               <Text alignItems="center" as="span">
                 Forrige
               </Text>
             </Button>
-          ) : (
-            <Box />
           )}
-          <Button variant="dark" w="245px" onClick={() => handleNavigation(activeTab + 1)}>
+          <Button variant="dark" w="245px" gridArea="2 / 2" onClick={() => handleNavigation(activeTab + 1)}>
             {activeTab !== 2 ? 'Neste' : 'Fullfør'}
           </Button>
         </Grid>
