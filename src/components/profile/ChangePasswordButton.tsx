ae122a4214d93ddb1d315232679488d52bdf9205
André Schjøth <aschjoth@gmail.com>
2022-04-22 23:59:34 +0200

    fix: navigation to Auth0

--- src/components/profile/ChangePasswordButton.tsx
+++ src/components/profile/ChangePasswordButton.tsx
@@ -1,19 +1,17 @@
 import { Button, Flex, useToast, Box, Text } from '@chakra-ui/react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useUpdateMyPasswordLazyQuery } from '../../__generated__/graphql-types';
 import Loading from '../common/Loading';
 
 const ChangePasswordButton: React.VFC = () => {
   const toast = useToast();
 
-  const navigate = useNavigate();
   const [updateMyPassword, { loading }] = useUpdateMyPasswordLazyQuery();
 
   const getUpdatePasswordLink = async () => {
     await updateMyPassword().then((res) => {
       res.data?.updateMyPassword
-        ? navigate(res.data?.updateMyPassword, { replace: true })
+        ? window.location.replace(res.data?.updateMyPassword)
         : toast({
             title: 'Det skjedde noe feil',
             isClosable: true,
