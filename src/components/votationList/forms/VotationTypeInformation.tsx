0272f9c0d8bc8aa5f0f52077d90b6acae776ae91
brynjarmorka <morkabrynjar+github@gmail.com>
2022-10-10 22:36:24 +0200

    Update description of Flertallsformer

--- src/components/votationList/forms/VotationTypeInformation.tsx
+++ src/components/votationList/forms/VotationTypeInformation.tsx
@@ -7,19 +7,26 @@ import ExternalLink from '../../common/buttons/ExternalLink';
 const VotationTypeInformation: React.FC = () => {
   return (
     <InformationModal title="Flertallsformer" ariaLabel="Informasjon om flertallsformer" alignWithText={true}>
+      <Text mb="1rem">
+        Administrator og teller får opp stemmefordelingen, og kan derfor selv regne ut et resultat basert på andre
+        regler enn de nedenfor.
+      </Text>
       <Heading size="sm">Simpelt flertall</Heading>
       <Text mb="1rem">
         Alternativet med flest stemmer vinner. <br />
-        Blanke stemmer telles ikke.
+        Blanke og avholdende stemmer telles ikke i utregningen.
       </Text>
-      <Heading size="sm">Kvalifisert flertall</Heading>
+      <Heading size="sm">Kvalifisert 50% flertall</Heading>
       <Text mb="1rem">
-        Et alternativ trenger stemmene fra mer enn halvparten av de stemmeberettigede tilstede for å vinne.
+        Et alternativ trenger stemmene fra mer enn halvparten av de stemmeberettigede tilstede for å vinne. <br />
+        Hvis alle med stemmerett er tilstede, er dette likt absolutt flertall. <br />
+        Både blanke og avholdende stemmer telles med i utregningen.
       </Text>
 
       <Heading size="sm">Kvalifisert 2/3 flertall</Heading>
       <Text mb="1rem">
-        Et alternativ trenger stemmene fra mer enn to tredjedeler av de stemmeberettigede tilstede for å vinne.
+        Et alternativ trenger stemmene fra mer enn to tredjedeler av de stemmeberettigede tilstede for å vinne. <br />
+        Både blanke og avholdende stemmer telles med i utregningen.
       </Text>
       <Heading size="sm">Preferansevalg</Heading>
       <Text mb="1rem">
